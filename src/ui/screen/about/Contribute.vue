90247e81a54b4ea9c760a8e3020a1bd0cde65c4d
Thomas Wajs <thomas.wajs@gmail.com>
2025-09-16 15:27:56 +0200

    Update the Contribute page

--- src/ui/screen/about/Contribute.vue
+++ src/ui/screen/about/Contribute.vue
@@ -2,12 +2,45 @@
     <div class="about-panel">
         <h1>Contribute</h1>
         <p>
-            The source code is not released yet, but you can contribute to the project by donating.
+            <strong>Source code</strong> : Freely available on
+            <a
+                href="https://github.com/thomasWajs/succubus-club/"
+                target="_blank"
+            >
+                GitHub
+            </a>
+            . Feel free to fork and contribute to the project.
         </p>
 
-        <p>A github repository will soon be available.</p>
+        <p>
+            <strong>Bug reports</strong> : Use the widget in the bottom right corner or through
+            <a
+                href="https://github.com/thomasWajs/succubus-club/issues/"
+                target="_blank"
+            >
+                GitHub issues
+            </a>
+        </p>
 
-        <p>Bug report can be made with the widget in the bottom right corner.</p>
+        <p>
+            <strong>Feature requests</strong> :
+            <a
+                href="https://github.com/thomasWajs/succubus-club/discussions"
+                target="_blank"
+            >
+                GitHub discussion
+            </a>
+        </p>
+
+        <p>
+            <strong>Want to chat ?</strong> :
+            <a
+                href="https://discord.com/users/502548674547810366"
+                target="_blank"
+            >
+                Reach out on Discord
+            </a>
+        </p>
 
         <h1>Donations</h1>
 
