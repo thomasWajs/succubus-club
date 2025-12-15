54efdeb1604a42928832f8f2eda0fc4881e22c5f
Thomas Wajs <thomas.wajs@gmail.com>
2025-12-15 17:01:54 +0100

    Update Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,18 @@
+## [0.5.3] - 2025-12-15
+
+Features:
+- Change sizing of Ready/Torpor/Uncontrolled areas
+- Reorganize context menu with infrequent commands
+- Lock/Unlock command
+- Invalid decklists is now a warning instead of error
+
+Bugfix:
+- Minion are always displayed on top of other cards
+- Fix drag over hand and play areas
+- Handle timer during reconnection
+- Fix Turn counter on oust
+- Remove counters when a card leav the play area
+
 ## [0.5.2] - 2025-12-09
 
 Features:
--- src/changelog.ts
+++ src/changelog.ts
@@ -1,14 +1,17 @@
 export const latestChangelog = {
-    version: '0.5.2',
-    date: '2025-12-09',
+    version: '0.5.3',
+    date: '2025-12-15',
     features: [
-        'Cards can be pinged',
-        'Shuffle lib/crypt on right click',
-        'Import a deck from Amaranth',
-        'All actions are now cancellable',
+        'Change sizing of Ready/Torpor/Uncontrolled areas',
+        'Reorganize context menu with infrequent commands',
+        'Lock/Unlock command',
+        'Invalid decklists is now a warning instead of error',
     ],
     bugfixes: [
-        'Card grouping is now opt-in instead of automatic',
-        'Fix stealth/intercept for the bot',
+        'Minion are always displayed on top of other cards',
+        'Fix drag over hand and play areas',
+        'Handle timer during reconnection',
+        'Fix Turn counter on oust',
+        'Remove counters when a card leav the play area',
     ],
 }
