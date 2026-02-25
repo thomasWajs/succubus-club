bfdfd3dc071b5f62d4888000dabf6387b60ce860
Thomas Wajs <thomas.wajs@gmail.com>
2026-02-25 11:42:14 +0100

    Update Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,20 @@
+## [0.6.2] - 2026-02-25
+
+Features:
+- Add an option at multiplayer game creation to disable aids
+- Pin a close up card by clicking it ( helps with looking at rulings )
+- Add deck explore to deck history
+- Burn from top of library/crypt
+- Use 2P rules for the bot
+
+Bugfix:
+- Address critical memory issues that slowed everything down in 0.6.1
+- Fix timer desync between players
+- Prevent log scrolling when not viewing the bottom
+- Fix Deck content counting
+- Prevent keyboard shortcuts to trigger when updating user preferences
+- Don't open pool panel when selecting a target
+
 ## [0.6.1] - 2026-01-30
 
 Features:
--- src/client/changelog.ts
+++ src/client/changelog.ts
@@ -1,12 +1,16 @@
 export const latestChangelog = {
-    version: '0.6.1',
-    date: '2026-01-30',
+    version: '0.6.2',
+    date: '2026-02-25',
     features: [
         'Add an option at multiplayer game creation to disable aids',
         'Pin a close up card by clicking it ( helps with looking at rulings )',
         'Add deck explore to deck history',
+        'Burn from top of library/crypt',
+        'Use 2P rules for the bot',
     ],
     bugfixes: [
+        'Address critical memory issues that slowed everything down in 0.6.1',
+        'Fix timer desync between players',
         'Prevent log scrolling when not viewing the bottom',
         'Fix Deck content counting',
         'Prevent keyboard shortcuts to trigger when updating user preferences',
