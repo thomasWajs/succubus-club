8013c80cfc8abba71bf98959c22a01404385858d
Thomas Wajs <thomas.wajs@gmail.com>
2026-09-03 21:20:24 +0200

    Fix yaml indentation config + remove semicolon

--- api/ablyAuth.mjs
+++ api/ablyAuth.mjs
@@ -1,14 +1,16 @@
 import Ably from 'ably'
 
 export async function POST(request) {
-    const ABLY_API_KEY = process.env.ABLY_API_KEY;
+    const ABLY_API_KEY = process.env.ABLY_API_KEY
 
     try {
         const ably = new Ably.Rest({ key: ABLY_API_KEY })
-        const tokenRequest = await ably.auth.createTokenRequest({ clientId: request.headers.get('clientId') })
-        return Response.json(tokenRequest )
+        const tokenRequest = await ably.auth.createTokenRequest({
+            clientId: request.headers.get('clientId'),
+        })
+        return Response.json(tokenRequest)
     } catch (e) {
-        console.error("error getting auth token from ably", e);
-        return Response.json({error: "error getting auth token from ably"}, {status: 500});
+        console.error('error getting auth token from ably', e)
+        return Response.json({ error: 'error getting auth token from ably' }, { status: 500 })
     }
 }
